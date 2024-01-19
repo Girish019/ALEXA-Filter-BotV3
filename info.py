aed5c16d27de691891c517749aac3d69d34f0377
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-20 02:10:14 +0530

    Update info.py

--- info.py
+++ info.py
@@ -97,8 +97,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://mj-filter-v01.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://mj-filter-v01.onrender.com/".format(FQDN, PORT)
+URL = "https://am-v3.onrender.com/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://am-v3.onrender.com/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '10'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'AMBot'))
