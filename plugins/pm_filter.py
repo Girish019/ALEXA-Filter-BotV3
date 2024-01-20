e1f985f30bd8ca39c1e8cc6622833908786c461f
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-20 15:31:49 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,11 @@ async def give_filter(client, message):
                 await save_group_settings(grpid, 'auto_ffilter', True)
                 settings = await get_settings(message.chat.id)
                 if settings['auto_ffilter']:
-                    await auto_filter(client, message) 
+                    await auto_filter(client, message)
+                else:
+                    button = [[InlineKeyboardButton("ᴄᴏɴᴛᴀᴄᴛ ᴀᴅᴍɪɴ", url=f"https://t.me/{SUPPORT_CHAT}")]]
+                    await message.reply_text("<b>sᴏᴍᴛʜɪɴɢ ᴡʀᴏɴɢ ᴘʟᴇᴀsᴇ ᴄᴏɴᴛᴇᴄᴛ ɢʀᴏᴜᴘ ᴀᴅᴍɪɴ ᴏʀ \n ᴅɪsᴄᴜss ᴛʜᴇ ǫᴜɪʀʏ ᴡɪᴛʜ ᴍʏ ᴀᴅᴍɪɴ </b>", reply_markup=InlineKeyboardMarkup(button))
+
     else: #a better logic to avoid repeated lines of code in auto_filter function
         search = message.text
         temp_files, temp_offset, total_results = await get_search_results(chat_id=message.chat.id, query=search.lower(), offset=0, filter=True)
