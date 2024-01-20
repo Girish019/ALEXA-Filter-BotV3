a7a6525a2f935ba0fa15940367c471a54c4fd5ab
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-21 00:25:03 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -33,4 +33,12 @@ async def premediaindex(bot, message):
 
     media.file_type = file_type
     media.caption = message.caption
-    await pre_dvd_savefile(media)
+    cout = await pre_dvd_savefile(media)
+    try:
+        count = cout.count(media)
+        await client.send_message(PREDVD_CHANNEL, f"pre DVD files are saved = {count}")
+    except:
+        await client.send_message(PREDVD_CHANNEL, "pre DVD files are saved in daatabase")
+
+
+
