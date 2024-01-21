f46f3e3182b29b2457bef1f63bd934df0c72813f
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-21 06:13:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -985,7 +985,7 @@ async def shortlink(bot, message):
         return await message.reply(f"You are anonymous admin. Turn off anonymous admin and try again this command")
     chat_type = message.chat.type
     if chat_type == enums.ChatType.PRIVATE:
-        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\n#Sample:-\n/shortlink upshrink.com c5dbea7703b576144630660355aaa3dbd3a59cc5\n\nThat's it!!! Enjoy Earning Money 💲\n\nIf you have any Doubts, Feel Free to Ask me - @MJ_supportbot\n\n(Puriyala na intha contact la message pannunga - @MJ_supportbot)</b>")
+        return await message.reply_text(f"<b>Hey {message.from_user.mention}, This command only works on groups !\n\n<u>Follow These Steps to Connect Shortener:</u>\n\n1. Add Me in Your Group with Full Admin Rights\n\n2. After Adding in Grp, Set your Shortener\n\nSend this command in your group\n\n—> /shortlink ""{your_shortener_website_name} {your_shortener_api}\n\n#Example:-\n/shortlink upshrink.com db8e518b1e15d2184e0dfe7467a6e6e92f4ccbf4\n\nThat's it!!! Enjoy Earning Money 💲\n\nIf you have any Doubts, Feel Free to Ask me - @AMsuporttbot</b>")
     elif chat_type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         grpid = message.chat.id
         title = message.chat.title
