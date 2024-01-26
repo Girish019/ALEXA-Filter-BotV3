7b052b9d61d733c3953dc083a63b46810c17a425
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-26 18:32:40 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -56,7 +56,7 @@ async def save_group(bot, message):
             a=await db.add_user(message.from_user.id, message.from_user.first_name)
             await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
         except:
-            await client.send_message(LOG_CHANNEL, f"{message.from_user.mention} is trying to connect new chat and he not started bot \n\nUSER ID : {message.from_user.id}\nCHAT ID : {message.chat.id}\nCHAT NAME :{message.chat.title}"))
+            await client.send_message(LOG_CHANNEL, f"{message.from_user.mention} is trying to connect new chat and he not started bot \n\nUSER ID : {message.from_user.id}\nCHAT ID : {message.chat.id}\nCHAT NAME :{message.chat.title}")
     else:
         settings = await get_settings(message.chat.id)
         if settings["welcome"]:
