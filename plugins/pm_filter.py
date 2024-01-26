ee8de7b5d6fb868653a44d04eadfe89c43ce717f
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-01-26 15:45:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -266,6 +266,7 @@ async def advantage_spoll_choker(bot, query):
             else:
                 reqstr1 = query.from_user.id if query.from_user else 0
                 reqstr = await bot.get_users(reqstr1)
+                await query.message.delete()
                 butn = [[InlineKeyboardButton("ᴄᴏɴᴛᴀᴄᴛ ᴀᴅᴍɪɴ", url=f"https://t.me/{SUPPORT_CHAT}")]]
                 k = await query.message.reply_to_message.reply_photo(
                 photo=REST_NOT_FND, 
