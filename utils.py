5ee8369823ef13ab8d508d1dfadbce557b75e524
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-07-23 19:35:20 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -529,42 +529,61 @@ async def get_tutorial(chat_id):
     else:
         TUTORIAL_URL = TUTORIAL
     return TUTORIAL_URL
-        
-async def get_verify_shorted_link(link):
+
+def get_verify_shorted_link(link):
     API = SHORTLINK_API
     URL = SHORTLINK_URL
     https = link.split(":")[0]
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-        url = f'https://{URL}/api'
-        params = {'api': API, 'url': link,}
+        length = random.randint(3, 8)
+        population = string.ascii_letters + string.digits
+        random_string = ''.join(random.choice(population) for _ in range(length))
         try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
-                    data = await response.json()
-                    if data["status"] == "success":
-                        return data['shortenedUrl']
-                    else:
-                        logger.error(f"Error: {data['message']}")
-                        return f'https://{URL}/api?api={API}&link={link}'
+            resp = requests.get(f"https://{SL_URL}/api?api={SL_API}&url={Tlink}&alias={random_string}")
+            data = resp.json()
+            url = data["shortenedUrl"]
+            return url
+        except Exception as e:
+            logger.error(e)
+            return f'{URL}/api?api={API}&link={link}'
+        
+# async def get_verify_shorted_link(link):
+#     API = SHORTLINK_API
+#     URL = SHORTLINK_URL
+#     https = link.split(":")[0]
+#     if "http" == https:
+#         https = "https"
+#         link = link.replace("http", https)
+#         url = f'https://{URL}/api'
+#         params = {'api': API, 'url': link,}
+#         try:
+#             async with aiohttp.ClientSession() as session:
+#                 async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+#                     data = await response.json()
+#                     if data["status"] == "success":
+#                         return data['shortenedUrl']
+#                     else:
+#                         logger.error(f"Error: {data['message']}")
+#                         return f'https://{URL}/api?api={API}&link={link}'
 
-        except:
-            length = random.randint(3, 8)
-            population = string.ascii_letters + string.digits
-            random_string = ''.join(random.choice(population) for _ in range(length))
-            try:
-                resp = requests.get(f"https://{URL}/api?api={API}&url={link}&alias={random_string}")
-                data = resp.json()
-                url = data["shortenedUrl"]
-                return url
-            except Exception as e:
-                logger.error(e)
-                return f'{URL}/api?api={API}&link={link}'
-    else:
-        return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
+#         except:
+#             length = random.randint(3, 8)
+#             population = string.ascii_letters + string.digits
+#             random_string = ''.join(random.choice(population) for _ in range(length))
+#             try:
+#                 resp = requests.get(f"https://{URL}/api?api={API}&url={link}&alias={random_string}")
+#                 data = resp.json()
+#                 url = data["shortenedUrl"]
+#                 return url
+#             except Exception as e:
+#                 logger.error(e)
+#                 return f'{URL}/api?api={API}&link={link}'
+#     else:
+#         return f'https://{URL}/shortLink?token={API}&format=json&link={link}'
 
-    #if URL == "api.shareus.in":
+#     #if URL == "api.shareus.in":
     #    url = f"https://{URL}/shortLink"
     #    params = {"token": API,
     #              "format": "json",
