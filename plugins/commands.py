fed8f6d8db96882118aeb7ee1c8132b22ca37480
Girish019 <118633556+Girish019@users.noreply.github.com>
2024-08-03 03:13:36 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -482,6 +482,7 @@ async def start(client, message):
     title = ' ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
     f_caption=files.caption
+    capt = files.caption
     ##########################################################################################################################################
     #get_cap = await write_cap(title, f_caption)
     #p2 = await client.send_message(chat_id=message.from_user.id, text=f"cap - {get_cap}\n\n filename - {title} \n caption - {f_caption}")
@@ -491,13 +492,11 @@ async def start(client, message):
             f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
             
             #f_caption = script.CUSTOM_FILE_CAP.format(file_name= '' if title is None else title, audio = 'NOT FOUND' if title is None else "English, hindi", file_size='' if size is None else size)
-            p2log = await client.send_message(chat_id=LOG_CHANNEL, text=f"filename - {title} \n caption - {f_caption}")
-            get_cap = write_cap(title, f_caption)
+            p2log = await client.send_message(chat_id=LOG_CHANNEL, text=f"filename - {title} \n caption - {capt}")
+            get_cap = write_cap(title, capt)
             print(get_cap)
-            p2lo = await client.send_message(chat_id=LOG_CHANNEL, text=f"getcap - {get_cap} \n\nfilename - {title} \n caption - {f_caption}")
+            p2lo = await client.send_message(chat_id=LOG_CHANNEL, text=f"getcap - {get_cap} \n\nfilename - {title} \n caption - {capt}")
         except Exception as e:
-            print(e)
-            p2log = await client.send_message(chat_id=LOG_CHANNEL, text=f"log - {e} \n\nfilename - {title} \n caption - {f_caption}")
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
